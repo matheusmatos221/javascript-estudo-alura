df71a59b87157744326453b7ab12739e34d32029
Matheus <matheusmatos.221@hotmail.com>
2022-01-10 18:46:09 -0300

    Install npm body-parser

--- 4-RestNodeJS/controllers/atendimentos.js
+++ 4-RestNodeJS/controllers/atendimentos.js
@@ -9,4 +9,9 @@ module.exports = app => {
     app.get('/atendimentos', (req, res) => {
         res.send('Você está na rota de ATENDIMENTOS e está realizando um GET')
     })
+    
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('Você está na rota de ATENDIMENTOS e está realizando um POST')
+    })
 }
