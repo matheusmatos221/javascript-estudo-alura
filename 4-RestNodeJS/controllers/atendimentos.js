f04f47445da13075de5c2dae7339926462123f36
Matheus <matheusmatos.221@hotmail.com>
2022-01-10 18:16:56 -0300

    Add comments to know where we call an module

--- 4-RestNodeJS/controllers/atendimentos.js
+++ 4-RestNodeJS/controllers/atendimentos.js
@@ -1,9 +1,11 @@
+// Exporta as funções
 module.exports = app => {
+    // Exporta as funções para APP
     app.get('/', (req, res) => {
         res.send('Hello World')
     })
     
     app.get('/atendimentos', (req, res) => {
-        res.send('Você está na rota de ATENDIMENTOS e está realizando um GETgi')
+        res.send('Você está na rota de ATENDIMENTOS e está realizando um GET')
     })
 }
--- 4-RestNodeJS/index.js
+++ 4-RestNodeJS/index.js
@@ -1,4 +1,8 @@
+// Importa módulos
 const express = require('express')
+const consign = require('consign')
+
+// Instânciação dos módulos
 const app = express()
 const port = 3000
 
