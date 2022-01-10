d3323926d02cf71a3f07d1861d798516f9410dab
Matheus <matheusmatos.221@hotmail.com>
2022-01-10 16:47:53 -0300

    Install consign using "npm install consign" comand

--- 4-RestNodeJS/controllers/atendimentos.js
+++ 4-RestNodeJS/controllers/atendimentos.js
@@ -1,7 +1,9 @@
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
-
-app.get('/atendimentos', (req, res) => {
-    res.send('Você está na rota de ATENDIMENTOS e está realizando um GETgi')
-})
+module.exports = app => {
+    app.get('/', (req, res) => {
+        res.send('Hello World')
+    })
+    
+    app.get('/atendimentos', (req, res) => {
+        res.send('Você está na rota de ATENDIMENTOS e está realizando um GETgi')
+    })
+}
