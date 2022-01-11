e26ec8d0e89029983cc69387588ceaa8f9dd5286
Matheus <matheusmatos.221@hotmail.com>
2022-01-10 23:57:06 -0300

    Add bodyParser on custom-express.js to receive urlencoded and json POST

--- 4-RestNodeJS/config/custom-express.js
+++ 4-RestNodeJS/config/custom-express.js
@@ -3,11 +3,16 @@
 // Importa módulos
 const express = require('express')
 const consign = require('consign')
+const bodyParser = require('body-parser') // Necessário para receber informações do tipo body com verbo POST
 
 module.exports = () => {
     // Instânciação dos módulos
     // "cria a variável 'app'"
     const app = express()
+
+    // Pede para o app usar o bodyParser
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
 
     // Configura servidor
     // "configura a variável 'app' com o 'consign'"
