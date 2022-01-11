d41ebba0199fdd2041c6581e9f036c38aebb2b99
Matheus <matheusmatos.221@hotmail.com>
2022-01-10 23:53:57 -0300

    Add const for set localhost port, default 3000

--- 4-RestNodeJS/index.js
+++ 4-RestNodeJS/index.js
@@ -1,5 +1,8 @@
 // Responsabilidade: Iniciar o servidor
 
+// Define uma variavel para declarar a porta
+const port = 3000
+
 // Importa módulos
 // "importa o express já configurado"
 const customExpress = require('./config/custom-express')
@@ -7,6 +10,6 @@ const customExpress = require('./config/custom-express')
 // Instancia o 'app' a partir do módulo 'customExpress'
 const app = customExpress()
 
-app.listen(3000, () => {
-    console.log(`Example app listening localhost 3000`)
+app.listen(port, () => {
+    console.log(`Example app listening localhost ${port}`)
 })
