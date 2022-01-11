c376d493e258c42857bef1a97e51527d9edae3c0
Matheus <matheusmatos.221@hotmail.com>
2022-01-11 15:11:23 -0300

    Add table constructor module into 'infraestrutura' and init when start server (index.js)

--- 4-RestNodeJS/index.js
+++ 4-RestNodeJS/index.js
@@ -9,14 +9,18 @@ const customExpress = require('./config/custom-express')
 
 // Conexao com banco de dados (Persistent)
 const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/tabelas')
 
 conexao.connect(erro => {
     console.log(`Tentando conexão com banco de dados...`)
     if(erro) {
         // Erro de conexao
+        console.log(`Falha de conexão com Banco de Dados!`)
         console.error(erro)
     } else {
         console.log(`BD conectado com sucesso!`)
+        Tabelas.init(conexao)
+
         // Instancia o 'app' a partir do módulo 'customExpress'
         const app = customExpress()
         app.listen(port, () => {
