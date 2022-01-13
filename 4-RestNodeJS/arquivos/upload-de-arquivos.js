f25546c0c49c6fbfa3fe4845da69f04ba8b5630c
Matheus <matheusmatos.221@hotmail.com>
2022-01-13 15:00:13 -0300

    PATCH upload-de-arquivos.js to use stream instead buffer and improve our memory management

--- 4-RestNodeJS/arquivos/upload-de-arquivos.js
+++ 4-RestNodeJS/arquivos/upload-de-arquivos.js
@@ -1,16 +1,6 @@
 // fs = FileSystem
 const fs = require('fs')
 
-fs.readFile('./assets/salsicha.jpg', (erro, buffer) => {
-    if(erro) {
-        console.error('ERRO');
-    } else {
-        console.log(buffer)
-        fs.writeFile('./assets/salsicha-2.jpg', buffer, (erro) => {
-            if(erro){
-                console.error(erro);
-            }
-        })
-    }
-})
-
+fs.createReadStream('./assets/salsicha.jpg')
+    .pipe(fs.createWriteStream('./assets/salsicha-stream.jpg'))
+    .on('finish', ()=> console.log('Imagem escrita com sucesso'))
