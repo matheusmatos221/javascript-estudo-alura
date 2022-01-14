5f95b3d0a6205759aa5cf5b7096729cbecfbb7ce
Matheus <matheusmatos.221@hotmail.com>
2022-01-13 23:07:11 -0300

    PATCH/FIX = If we have an error in upload-de-arquivos.js, send error on callback function and answer with status 400

--- 4-RestNodeJS/models/pets.js
+++ 4-RestNodeJS/models/pets.js
@@ -6,18 +6,23 @@ class Pet{
     adiciona(pet, res){
         const sql = 'INSERT INTO pets SET ?'
 
-        uploadDeArquivo(pet.imagem, pet.nome, (novoCaminho) => {
-            const novoPet = {nome: pet.nome, imagem:novoCaminho}
-            
-            conexao.query(sql, novoPet, (erro, resultado)=>{
-                if(erro){
-                    console.error(erro)
-                    res.status(400).json(erro)
-                } else {
-                    res.status(200).json(novoPet)
-                }
-            }) 
+        uploadDeArquivo(pet.imagem, pet.nome, (erro, novoCaminho) => {
+            if(erro){
+                res.status(400).json({erro})
+            } else {
+                const novoPet = {nome: pet.nome, imagem:novoCaminho}
+                
+                conexao.query(sql, novoPet, (erro, resultado)=>{
+                    if(erro){
+                        console.error(erro)
+                        res.status(400).json(erro)
+                    } else {
+                        res.status(200).json(novoPet)
+                    }
+                })        
+            }
         })
+
     }
 }
 
