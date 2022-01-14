8855b61b66d74f537e94e007dcf0459251165e7b
Matheus <matheusmatos.221@hotmail.com>
2022-01-13 22:48:53 -0300

    PATCH - Accept only few formats on upload-de-arquivos

--- 4-RestNodeJS/arquivos/upload-de-arquivos.js
+++ 4-RestNodeJS/arquivos/upload-de-arquivos.js
@@ -3,11 +3,18 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) =>{
+    const tiposValidos = ['jpg', 'png', 'jpeg']
     const tipoArquivo = path.extname(caminho)
-    const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipoArquivo}` // Caminho do diretório interpolado com nome do arquivo
+    const tipoEhValido = tiposValidos.indexOf(tipoArquivo.substring(1)) // Retira o ponto do formato '.jpg' fica 'jpg'
 
-    fs.createReadStream(caminho) // Lê o arquivo que está no caminho
-        .pipe(fs.createWriteStream(novoCaminho)) // Escreve no novo caminho
-        .on('finish', ()=> callbackImagemCriada(novoCaminho)) // Ao terminar, chama uma função callback a ser passada via parâmetro
+    if(tipoEhValido === -1){
+        console.error('Erro! Tipo inválido')
+    } else {
+        const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipoArquivo}` // Caminho do diretório interpolado com nome do arquivo
+    
+        fs.createReadStream(caminho) // Lê o arquivo que está no caminho
+            .pipe(fs.createWriteStream(novoCaminho)) // Escreve no novo caminho
+            .on('finish', ()=> callbackImagemCriada(novoCaminho)) // Ao terminar, chama uma função callback a ser passada via parâmetro
+    }
 }
 
